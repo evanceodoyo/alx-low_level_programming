13b86e6dbcfabcdf50abf82b2e97db1883325250
Evance Odoyo <odoyoevanc@gmail.com>
2022-09-14 16:23:42 -0700

    Added 3-islower.c, 4-isalpha.c, 5-sign.c and 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @n: integer to check
+ *
+ * Return: n
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+	{
+		n *= -1;
+		return (n);
+	}
+}
