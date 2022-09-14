b47a5098ebc84ee4ebb2a7604a647bdc20c69fa1
Evance Odoyo <odoyoevanc@gmail.com>
2022-09-14 15:08:08 -0700

    Added File: 1-alphabet.c, 2-print_alphabet_x10.c, and updated the README

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#included "main.h"
+
+/**
+ * Print the alphabet 10 times in lowercase.
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	while (i <= 9)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
