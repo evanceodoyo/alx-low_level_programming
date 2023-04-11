745b4e580b35665bb26c763743caaba694605cf0
Evance Odoyo <odoyoevanc@gmail.com>
2023-04-11 23:40:06 +0300

    Add header files

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+/*
+ * Desc: Header file containing all function prototypes.
+ *	 for the 0x1E-search_algorithms project.
+ */
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif /* SEARCH_ALGOS_H*/
