de0e0b6a6d9a335af15a1b6271fabb4233864317
Evance Odoyo <odoyoevanc@gmail.com>
2023-04-14 23:50:33 +0300

    Worst case time complexity for linear search

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for value in an array using binary search algorithm
+ * @array: pointer to the first element of the array to search in.
+ * @size: number of elements in array
+ * @value: value to search for.
+ *
+ * Return: index of value; if array is NULL or value not in array, return -1
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t val;
+}
